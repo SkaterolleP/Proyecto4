5e98a6cedbe75471a84ce4f94bc7646c4d7ef72e
usuario <usuario@pc21-081>
2019-11-05 18:08:42 +0100

    Clase Cuenta implementada para que se compile solamente

---
+++ src/Cuenta/cuenta.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cuenta;
+
+/**
+ *
+ * @author usuario
+ */
+public class cuenta {
+    
+    private static double saldo;
+    
+    public static double getSaldo(){
+        return 0;
+    }
+    
+    public static void ingresar(double x){
+        saldo = 0;
+    }
+    
+    public static void retirar(double x){
+        saldo = 0;
+    }
+}
